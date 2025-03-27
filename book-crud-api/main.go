084a7fc4f39fec13311171211d61308b76511934
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-03-27 16:15:02 +0530

    changed the project directory and make the GET request online

---
+++ book-crud-api/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "book-crud-api/handlers"
+)
+
+func main() {
+    http.HandleFunc("/books", handlers.HandleBooks)
+    log.Println("Server running on http://localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
